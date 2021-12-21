c664878ccf1e0fd44a5380a16dca775431da417e
Sri Harsha <kudoark@gmail.com>
2021-12-21 16:03:56 +0530

    kde desktop with nordic theme

---
+++ kde_desktop.sh
@@ -0,0 +1,207 @@
+#!/usr/bin/env bash
+#-----------------------------------------------------------------------
+#   █████████                                            █████         
+#  ███░░░░░███                                          ░░███          
+# ███     ░░░  ████████  █████ ████  █████   ██████   ███████   ██████ 
+#░███         ░░███░░███░░███ ░███  ███░░   ███░░███ ███░░███  ███░░███
+#░███          ░███ ░░░  ░███ ░███ ░░█████ ░███████ ░███ ░███ ░███ ░███
+#░░███     ███ ░███      ░███ ░███  ░░░░███░███░░░  ░███ ░███ ░███ ░███
+# ░░█████████  █████     ░░████████ ██████ ░░██████ ░░████████░░██████ 
+#  ░░░░░░░░░  ░░░░░       ░░░░░░░░ ░░░░░░   ░░░░░░   ░░░░░░░░  ░░░░░░  
+#-------------------------------------------------------------------------
+pacman -Sy --noconfirm
+
+echo "-------------------------------------------------"
+echo "          Installing additional Packages         "
+echo "-------------------------------------------------"
+PKGS=(
+'mesa' # Essential Xorg First
+'xorg'
+'xorg-server'
+'xorg-apps'
+'xorg-drivers'
+'xorg-xkill'
+'xorg-xinit'
+'xterm'
+'plasma-desktop' # KDE Load second
+'alsa-plugins' # audio plugins
+'alsa-utils' # audio utils
+'ark' # compression
+'audiocd-kio'
+'bluedevil'
+'breeze'
+'breeze-gtk'
+'bridge-utils'
+'celluloid' # video players
+'code' # Visual Studio code
+'dolphin'
+'dtc'
+'egl-wayland'
+'exfat-utils'
+'extra-cmake-modules'
+'filelight'
+'flex'
+'fuse2'
+'fuse3'
+'fuseiso'
+'gamemode'
+'gcc'
+'gimp' # Photo editing
+'gparted' # partition management
+'gst-libav'
+'gst-plugins-good'
+'gst-plugins-ugly'
+'gwenview'
+'haveged'
+'iptables-nft'
+'jdk-openjdk' # Java 17
+'kate'
+'kcodecs'
+'kcoreaddons'
+'kdeplasma-addons'
+'kde-gtk-config'
+'kinfocenter'
+'kscreen'
+'kvantum-qt5'
+'kitty'
+'konsole'
+'kscreen'
+'layer-shell-qt'
+'libdvdcss'
+'lsof'
+'lutris'
+'lzop'
+'m4'
+'milou'
+'ntfs-3g'
+'okular'
+'openbsd-netcat'
+'oxygen'
+'p7zip'
+'patch'
+'picom'
+'pkgconf'
+'plasma-meta'
+'plasma-nm'
+'powerdevil'
+'powerline-fonts'
+'print-manager'
+'pulseaudio'
+'pulseaudio-alsa'
+'pulseaudio-bluetooth'
+'python-notify2'
+'python-psutil'
+'python-pyqt5'
+'python-pip'
+'qemu'
+'rsync'
+'sddm'
+'sddm-kcm'
+'snapper'
+'spectacle'
+'steam'
+'swtpm'
+'synergy'
+'systemsettings'
+'terminus-font'
+'ufw'
+'usbutils'
+'vim'
+'virt-manager'
+'virt-viewer'
+'wine-gecko'
+'wine-mono'
+'winetricks'
+'xdg-desktop-portal-kde'
+'xdg-user-dirs'
+'zeroconf-ioslave'
+)
+#installing additional packages
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING: ${PKG}"
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
+if lspci | grep -E "NVIDIA|GeForce"; then
+    nvidia-xconfig	
+fi
+
+echo -e "\nINSTALLING AUR SOFTWARE\n"
+# You can solve users running this script as root with this and then doing the same for the next for statement. However I will leave this up to you.
+
+echo "CLONING: YAY"
+cd ~
+git clone "https://aur.archlinux.org/yay.git"
+cd ${HOME}/yay
+makepkg -si --noconfirm
+cd ~
+PKGS=(
+'autojump'
+'brave-bin' # Brave Browser
+'dxvk-bin' # DXVK DirectX to Vulcan
+'github-desktop-bin' # Github Desktop sync
+'ocs-url' # install packages from websites
+'snap-pac'
+)
+
+for PKG in "${PKGS[@]}"; do
+    yay -S --noconfirm $PKG
+done
+
+read themeinstall
+
+if [[$themeinstall =~ nordic]]; then
+PKGS=(
+'awesome-terminal-fonts'
+'lightly-git'
+'lightlyshaders-git'
+'mangohud' # Gaming FPS Counter
+'mangohud-common'
+'nerd-fonts-fira-code'
+'nordic-darker-standard-buttons-theme'
+'nordic-darker-theme'
+'nordic-kde-git'
+'nordic-theme'
+'noto-fonts-emoji'
+'papirus-icon-theme'
+'plasma-pa'
+'sddm-nordic-theme-git'
+'snapper-gui-git'
+'ttf-droid'
+'ttf-hack'
+'ttf-meslo' # Nerdfont package
+'ttf-roboto'
+)
+
+for PKG in "${PKGS[@]}"; do
+    yay -S --noconfirm $PKG
+done
+
+touch "$HOME/.cache/zshhistory"
+git clone "https://github.com/ChrisTitusTech/zsh"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
+ln -s "$HOME/zsh/.zshrc" $HOME/.zshrc
+
+export PATH=$PATH:~/.local/bin
+cp -r $HOME/KdeInstall/dotfiles/* $HOME/.config/
+pip install konsave
+konsave -i $HOME/KdeInstall/kde.knsv
+sleep 1
+konsave -a kde
+else
+export PATH=$PATH:~/.local/bin
+fi
+
+
+
+if lspci | grep -E "NVIDIA|GeForce"; then
+    nvidia-xconfig	
+fi
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo -e "\nEnabling Login Display Manager"
+systemctl enable sddm.service
+echo -e "\nSetup SDDM Theme"
+cat <<EOF > /etc/sddm.conf
+[Theme]
+Current=Nordic
+EOF
